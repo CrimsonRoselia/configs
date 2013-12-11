129720400df76bb64044bac16438c279459174a0
MrBiohazard <notbiohazard@zoho.com>
2013-12-11 20:47:57 +0100

    Woo, update

--- .weechat/python/prism.py
+++ .weechat/python/prism.py
@@ -8,7 +8,15 @@
 # TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 #
 # 0. You just DO WHAT THE FUCK YOU WANT TO.
-#
+# 2013-11-26, Seganku <seganku@zenu.net>
+#    v0.2.7: add -c switch for the option to pass output to a command
+# 2013-07-19, Sebastien Helleu <flashcode@flashtux.org>
+#    v0.2.6: use buffer received in command callback instead of current buffer
+# 2013-05-04, Rylai
+#    v0.2.5: add -e switch for the option to destroy the eyes of all
+#            who have the misfortune of seeing your text
+# 2013-04-26, Biohazard
+#   v0.2.4: add support for using the command through keybindings
 # 2013-03-12, R1cochet
 #   v0.2.3: add -b switch for backwards/reverse text
 # 2013-01-29, SuperT1R:
@@ -22,7 +30,7 @@ import re
 
 SCRIPT_NAME    = "prism"
 SCRIPT_AUTHOR  = "Alex Barrett <al.barrett@gmail.com>"
-SCRIPT_VERSION = "0.2.3"
+SCRIPT_VERSION = "0.2.7"
 SCRIPT_LICENSE = "WTFPL"
 SCRIPT_DESC    = "Taste the rainbow."
 
@@ -47,44 +55,64 @@ if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
               SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
     w.hook_command("prism",
                    SCRIPT_DESC,
-                   "[-rwmb] text",
+                   "[-rwmbe] text|-c[wbe] <sep> <command> <sep>text",
                    "    -r: randomizes the order of the color sequence\n"
                    "    -w: color entire words instead of individual characters\n"
                    "    -m: append /me to beginning of output\n"
                    "    -b: backwards text (entire string is reversed)\n"
+                   "    -e: eye-destroying colors (randomized background colors)\n"
+                   "    -c: specify a separator to turn on colorization\n"
+                   "        eg. -c : /topic :howdy howdy howdy\n"
                    "  text: text to be colored",
-                   "-r|-w|-m|-b", "prism_cmd_cb", "")
-
+                   "-r|-w|-m|-b|-e|-c", "prism_cmd_cb", "")
+def find_another_color(colorCode):
+    otherColor = (unicode(colors[random.randint(1, color_count - 1) % color_count]).rjust(2, "0"))
+    while (otherColor == colorCode):
+        otherColor = (unicode(colors[random.randint(1, color_count - 1) % color_count]).rjust(2, "0"))
+    return otherColor
 
 def prism_cmd_cb(data, buffer, args):
     global color_index
 
     input = args.decode("UTF-8")
+    input_method = "command"
+
+    if not input:
+        input = w.buffer_get_string(buffer, "input")
+        input = input.decode("UTF-8")
+        input_method = "keybinding"
 
     # select a tokenizer and increment mode
     regex = regex_chars
-    inc = 1
-    mepfx = 0
-
-    m = re.match('-[rwmb]* ', input)
-    if m:
-        opts = m.group(0)
-        input = input[len(opts):]
+    inc   = 1
+    bs    = 0
+    cmd   = ""
+    m = re.match(r'(-[rwmbec]+)\s+(?:([^ ]+)\s+(.+?)\s*\2)?(.*)', input)
+    if m and input_method == "command":
+        opts = m.group(1)
+        input = m.group(4)
+        if 'c' in opts:
+            cmd = m.group(3)
         if 'w' in opts:
             regex = regex_words
         if 'r' in opts:
             inc = 0
         if 'm' in opts:
-            mepfx = 1
+            cmd = "/me"
         if 'b' in opts:
             input = input[::-1]
+        if 'e' in opts:
+             bs = 1
 
     output = u""
     tokens = re.findall(regex, input)
     for token in tokens:
         # prefix each token with a color code
         color_code = unicode(colors[color_index % color_count]).rjust(2, "0")
-        output += u"\x03" + color_code  + token
+        if bs == 1:
+            output += u'\x03' + color_code + ',' + find_another_color(color_code) + token
+        else:
+            output += u"\x03" + color_code  + token
 
         # select the next color or another color at
         # random depending on the options specified
@@ -95,10 +123,13 @@ def prism_cmd_cb(data, buffer, args):
 
     # output starting with a / will be executed as a
     # command unless we escape it with a preceding /
+    # Commands should use the -c flag
     if len(output) > 0 and output[0] == "/":
         output = "/" + output
-    if mepfx == 1:
-        output = "/me " + output
-
-    w.command(w.current_buffer(), output.encode("UTF-8"))
+    if len(cmd) > 0:
+        output = cmd + ' ' + output
+    if input_method == "keybinding":
+        w.buffer_set(buffer, "input", output.encode("UTF-8"))
+    else:
+        w.command(buffer, output.encode("UTF-8"))
     return w.WEECHAT_RC_OK
